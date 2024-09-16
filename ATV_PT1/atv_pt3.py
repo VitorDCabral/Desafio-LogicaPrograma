72725ef8330a5bf6f4efeb0243afc860073364fc
VitorDCabral <168486899+VitorDCabral@users.noreply.github.com>
2024-09-16 18:56:11 -0300

    Add files via upload

---
+++ ATV_PT1/atv_pt3.py
@@ -0,0 +1,5 @@
+idade = int(input("Digite a sua idade:"))
+nome = input("Digite o seu nome:")
+cidade = input("Digite o nome da sua cidade natal:")
+
+print(f"Olá {nome} você tem {idade} anos e sua cidade natal é: {cidade}.")
